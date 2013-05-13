630c963bb5092c023f27a921ceab6437b0578f45
khadija ali <khadija.ali5@gmail.com>
2013-05-13 06:20:03 -0400

    CREATING THE APP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SST::Application.config.secret_token = '4d747834522f2d36f30aa94c38b7ee2a1990dcf16b1fb0bbd71e8a5b0c369b1bed5b3349a1cc870fdd4fa2017dcec67fa7bd98f344969cf18af9d6e44790a205'
